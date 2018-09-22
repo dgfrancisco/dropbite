8e502c6b6d4d566b07b45c74e12ffc53d4318810
Francisco Rodriguez <yodgfrancisco@gmail.com>
2018-09-21 21:16:50 -0500

    Commit number #1

---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+import Layout from '../components/layout'
+
+const IndexPage = () => (
+  <Layout>
+    <h1>Learn to design and code 2</h1>
+    <p>Welcome to your new Gatsby site.</p>
+    <Link to="/page-2/">Link</Link>
+  </Layout>
+)
+
+export default IndexPage
